bce3d23afb4eb504411fa56a10d621f421d5d77e
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-03-06 23:13:42 +0700

    resize

--- components/Loader.js
+++ components/Loader.js
@@ -4,8 +4,8 @@ const Loader = () => {
     return (<Image
         src="/loader.svg"
         alt="Picture of the author"
-        width={500}
-        height={500}
+        width={150}
+        height={150}
     />);
 }
 
